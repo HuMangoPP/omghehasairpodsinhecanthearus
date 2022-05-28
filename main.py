ccddc4469b30a9da4a65f9a016b1c5825fe0ca33
Mike Lee <supahotboi69@gmail.com>
2022-05-28 19:53:01 -0400

    created pygame window

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from src.settings import *
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption("personal_gamejam_1")
+    display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock();
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        display_surface.fill('black')
+        pygame.display.update()    
+        clock.tick(FPS)
